f3d5a6728e4f01a60ff40962de89d30eefcaa30a
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-03-28 09:56:34 +0600

    Use parallel renderer

--- src/extractors/channel.ts
+++ src/extractors/channel.ts
@@ -1,11 +1,26 @@
 import { load } from 'cheerio';
 import { sendRequest } from '../request';
-import type { ViewCountText, YouTubeScriptResponse } from './channel.types';
+import type {
+  Thumbnail,
+  ViewCountText,
+  YouTubeScriptResponse,
+} from './channel.types';
 import { isStream } from './utils';
 
 const baseUrl = 'https://www.youtube.com/';
 
-export const getVideos = async (channelId: string) => {
+export const getVideos = async (
+  channelId: string
+): Promise<
+  {
+    title?: string;
+    thumbnails?: Thumbnail[];
+    thumbnailUrl?: string;
+    length?: string;
+    publishedTime?: string;
+    viewCount?: number;
+  }[]
+> => {
   try {
     if (!channelId.startsWith('@')) {
       throw new Error(
@@ -15,139 +30,123 @@ export const getVideos = async (channelId: string) => {
     const url = `${baseUrl}/${channelId}/videos`;
 
     const data = await sendRequest(url);
-    const $ = load(data);
-
-    const scriptTags = $('script');
 
     const regex = /var\s+ytInitialData\s+=\s+({.*?});/;
-
-    const videosArray: {
-      title?: string;
-      videoId?: string;
-      thumbnailUrl?: string;
-      publishedTime?: string;
-      length: string;
-      viewCount: number;
-    }[] = [];
-
-    for (let i = 0; i < scriptTags.length; i++) {
-      const scriptContent = $(scriptTags[i]).html();
-      if (scriptContent) {
-        const match = scriptContent.match(regex);
-        if (match && match[1]) {
-          const videos = (JSON.parse(match[1]) as YouTubeScriptResponse)
-            ?.contents?.twoColumnBrowseResultsRenderer.tabs[1].tabRenderer
-            .content.richGridRenderer.contents;
-
-          videos?.forEach((videoItem) => {
-            const videoData =
-              videoItem?.richItemRenderer?.content?.videoRenderer;
-            if (!videoData) return;
-
-            const title = videoData.title?.runs[0]?.text;
-            const videoId = videoData.videoId;
-            const thumbnailUrl = videoData.thumbnail?.thumbnails.find(
-              (t) => t.width >= 300
-            )?.url;
-            const publishedTime = videoData.publishedTimeText?.simpleText;
-            const length = videoData.lengthText?.simpleText;
-            const viewCount = Number(
-              (videoData.viewCountText as ViewCountText)?.simpleText
-                .match(/[0-9,]+/g)?.[0]
-                .replace(/,/g, '')
-            );
-
-            videosArray.push({
-              title,
-              thumbnailUrl,
-              length,
-              publishedTime,
-              videoId,
-              viewCount,
-            });
-          });
-        }
-      }
+    const match = data.match(regex);
+    if (!match || !match[1]) {
+      throw new Error('Failed to parse video data');
     }
 
-    return videosArray;
+    const videos = JSON.parse(match[1]).contents?.twoColumnBrowseResultsRenderer
+      .tabs[1].tabRenderer.content.richGridRenderer.contents;
+
+    if (!videos) {
+      throw new Error('No videos found');
+    }
+
+    const videosArray = await Promise.all(
+      videos.map(async (videoItem: any) => {
+        const videoData = videoItem?.richItemRenderer?.content?.videoRenderer;
+        if (!videoData) return null;
+
+        const title = videoData.title?.runs[0]?.text;
+        const videoId = videoData.videoId;
+        const thumbnails = videoData.thumbnail?.thumbnails;
+        const thumbnailUrl = videoData.thumbnail?.thumbnails.find(
+          (t: any) => t.width >= 300
+        )?.url;
+        const publishedTime = videoData.publishedTimeText?.simpleText;
+        const length = videoData.lengthText?.simpleText;
+        const viewCount = Number(
+          (videoData.viewCountText as ViewCountText)?.simpleText
+            .match(/[0-9,]+/g)?.[0]
+            .replace(/,/g, '')
+        );
+
+        return {
+          title,
+          thumbnails,
+          thumbnailUrl,
+          length,
+          publishedTime,
+          videoId,
+          viewCount,
+        };
+      })
+    );
+
+    return videosArray.filter((video: any) => video !== null);
   } catch (error) {
     console.error('Error in getVideos:', error);
     throw error;
   }
 };
 
-export const getStreams = async (channelId: string) => {
+export const getStreams = async (
+  channelId: string
+): Promise<
+  {
+    title?: string;
+    thumbnails?: Thumbnail[];
+    thumbnailUrl?: string;
+    length?: string;
+    publishedTime?: string;
+    viewCount?: number;
+  }[]
+> => {
   try {
     if (!channelId.startsWith('@')) {
       throw new Error(
         "Invalid channel Id. Channel name must start with '@' i.e @mistahfeet"
       );
     }
-    const url = `${baseUrl}/${channelId}/streams`;
+    const url = `${baseUrl}/${channelId}/videos`;
 
     const data = await sendRequest(url);
-    const $ = load(data);
-
-    const scriptTags = $('script');
 
     const regex = /var\s+ytInitialData\s+=\s+({.*?});/;
-
-    const videosArray: {
-      title?: string;
-      videoId?: string;
-      thumbnailUrl?: string;
-      publishedTime?: string;
-      length: string;
-      viewCount: number;
-    }[] = [];
-
-    for (let i = 0; i < scriptTags.length; i++) {
-      const scriptContent = $(scriptTags[i]).html();
-      if (scriptContent) {
-        const match = scriptContent.match(regex);
-        if (match && match[1]) {
-          const videos = (JSON.parse(match[1]) as YouTubeScriptResponse)
-            ?.contents?.twoColumnBrowseResultsRenderer.tabs[3].tabRenderer
-            .content.richGridRenderer.contents;
-
-          videos?.forEach((videoItem) => {
-            const videoData =
-              videoItem?.richItemRenderer?.content?.videoRenderer;
-            if (!videoData) return;
-
-            const title = videoData.title?.runs[0]?.text;
-            const videoId = videoData.videoId;
-            const thumbnailUrl = videoData.thumbnail?.thumbnails.find(
-              (t) => t.width >= 300
-            )?.url;
-            const publishedTime = videoData.publishedTimeText?.simpleText;
-            const length = videoData.lengthText?.simpleText;
-            const viewCount = isStream(videoData)
-              ? Number(
-                  (videoData.viewCountText as { runs: { text: string }[] })
-                    .runs[0].text
-                )
-              : Number(
-                  (videoData.viewCountText as ViewCountText)?.simpleText
-                    .match(/[0-9,]+/g)?.[0]
-                    .replace(/,/g, '')
-                );
-
-            videosArray.push({
-              title,
-              thumbnailUrl,
-              length,
-              publishedTime,
-              videoId,
-              viewCount,
-            });
-          });
-        }
-      }
+    const match = data.match(regex);
+    if (!match || !match[1]) {
+      throw new Error('Failed to parse video data');
     }
 
-    return videosArray;
+    const videos = JSON.parse(match[1]).contents?.twoColumnBrowseResultsRenderer
+      .tabs[3].tabRenderer.content.richGridRenderer.contents;
+
+    if (!videos) {
+      throw new Error('No videos found');
+    }
+
+    const videosArray = await Promise.all(
+      videos.map(async (videoItem: any) => {
+        const videoData = videoItem?.richItemRenderer?.content?.videoRenderer;
+        if (!videoData) return null;
+
+        const title = videoData.title?.runs[0]?.text;
+        const videoId = videoData.videoId;
+        const thumbnailUrl = videoData.thumbnail?.thumbnails.find(
+          (t: any) => t.width >= 300
+        )?.url;
+        const publishedTime = videoData.publishedTimeText?.simpleText;
+        const length = videoData.lengthText?.simpleText;
+        const viewCount = Number(
+          (videoData.viewCountText as ViewCountText)?.simpleText
+            .match(/[0-9,]+/g)?.[0]
+            .replace(/,/g, '')
+        );
+
+        return {
+          title,
+          thumbnailUrl,
+          length,
+          publishedTime,
+          videoId,
+          viewCount,
+        };
+      })
+    );
+
+    return videosArray.filter((video: any) => video !== null);
   } catch (error) {
     console.error('Error in getStreams:', error);
     throw error;
